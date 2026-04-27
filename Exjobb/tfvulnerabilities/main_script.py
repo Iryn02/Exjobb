2e4ed853a0e2155b78b9dee8ded47043f1cc1d4d
Iryn02 <ida@rynger.se>
2026-04-27 12:11:00 +0200

    nytt

--- Exjobb/tfvulnerabilities/main_script.py
+++ Exjobb/tfvulnerabilities/main_script.py
@@ -2,18 +2,17 @@
 # Bachelor of Network Engineering - MDU University
 # Thesis work: Lateral Movement in Kubernetes Clusters
 
+import os
 import search_files as sf
 import subprocess as sp
-import os
 
 
-class Main_Gui:
+class EnvManager:
     installed_list = ['terraform','kubectl']
-    def __init__(self, input):
-        self.input = input
-    
-    
-    def check_dependencies(self): # check if needed programs are installed
+    def __init__(self, choice):
+        self.choice = choice
+
+    def check_dependencies(self):# check if needed programs are installed
         results = {}
         for package in self.installed_list:
             result = sp.run(
@@ -22,7 +21,7 @@ class Main_Gui:
                 stderr=sp.PIPE
             )
             results[package] = result.returncode == 0
-    
+
         for package, installed in results.items():
             if installed:
                 print(f'package {package} alredy installed')
@@ -36,11 +35,11 @@ class Main_Gui:
             sp.run(['sudo', 'snap', 'install', 'multipass'])
         except:
             print('Multipass alredy exists')
-            
+
     def fetch_mainfest(self, choice): #get manifest files for correct assignment
         ENVIRONMENTS = {'1' : '/home/$USER/Exjobb/scripts/Base/',
                         '2' : '/home/$USER/Exjobb/scripts/Hostpath/',
-                        '3' : '/home/$USER/Exjobb/scripts/managed_identity/', 
+                        '3' : '/home/$USER/Exjobb/scripts/managed_identity/',
                         '4' : '/home/$USER/Exjobb/scripts/Service_account/',
                         '5' : '/home/$USER/Exjobb/scripts/',
                         '6' : '/home/$USER/Exjobb/scripts/'}
@@ -50,35 +49,34 @@ class Main_Gui:
             for filename in ['main.tf', 'outputs.tf']:
                 with open(os.path.join(base_path, filename), 'r') as f:
                     contents[filename] = f.read()
-                    
-            result = sp.run(['terraform', 'init'], 
+
+            result = sp.run(['terraform', 'init'],
                             cwd=base_path,
                             capture_output=True,
                             text=True)
             print(result.stderr, result.stdout)
             if result.returncode == 0:
                 result = sp.run(['terraform', 'apply', '-auto-approve'],
-                                cwd=base_path, 
+                                cwd=base_path,
                                 capture_output=True,
                                 text=True)
                 print(result.stderr, result.stdout)
                 if result.returncode == 0:
-                    sp.run(['bash','/home/terra/git/exjobb/scripts/join_workers.sh']) #filväg innan join_workers
+                    sp.run(['bash','/home/terra/git/exjobb/scripts/join_workers.sh'])
             if choice == '5':
                 sp.run([sf])
             if choice == '6':
                 sp.run(['sudo','bash','/home/terra/git/exjobb/scripts/cleanup.sh'])
-                
+
             else:
                 print('Terraform error', result.stderr, result.stdout)
-                
+
             return contents
-        except: 
+        except:
             print('terraform error', result.stderr, result.stdout)
-    
+
     #def run_terraform(self): #terraform init/apply...
      #   result = sp.run(['terraform','init'], cwd=s
-        
      #   pass
 
     def gui(self):  #all prints and inputs from gui
@@ -98,6 +96,5 @@ class Main_Gui:
         self.fetch_mainfest(choice)
 
 def main():
-    test = Main_Gui()
+    test = vulnerabilities_program()
     test.gui()
-     
