5ac25b175639798c67c5a1c7a29f9a5634f26c8e
oqult <johnny.norrman@hotmail.com>
2026-05-07 13:28:44 +0200

    added shebang in search_files and main_script, also fixed paths to join_workers.sh in vulnerable environments

--- tfvulnerabilities/main_script.py
+++ tfvulnerabilities/main_script.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Lateral Movement Lab — Environment Manager
 ==========================================
--- tfvulnerabilities/search_files.py
+++ tfvulnerabilities/search_files.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Lateral Movement Lab — Security File Scanner
 ============================================
